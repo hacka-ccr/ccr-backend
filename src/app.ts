5b07a46f455553bc8de91f6533510f62bf5e9842
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-01-16 09:11:02 -0300

    feat: add routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import router from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Test route');
-});
+app.use(router);
 
 export default app;
